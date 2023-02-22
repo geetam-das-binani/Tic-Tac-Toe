9d06d52c29f07891d1b2caedcb4f649cf027112c
Geetam <geetambinani6@gmail.com>
2023-02-22 21:09:06 +0530

    second commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let turn = "X";
-// let audioturn= new Audio("ting.mp3");
-// let gameover=new Audio("gameover.mp3");
-// let music=new Audio("music.mp3");
+let audioturn= new Audio("ting.mp3");
+let gameover=new Audio("gameover.mp3");
+let music=new Audio("music.mp3");
 let isgameover = false;
 
 
@@ -14,40 +14,39 @@ let changeturn = () => {
     else {
         return "X"
     }
+    
 }
 
 let checkforwin = () => {
     let boxtext = document.querySelectorAll(".boxtext")
-    let wins = [[0, 1, 2, 9, 180, 7,],
-    [3, 4, 5, 8, 180, 20],
-    [6, 7, 8, 8, 180, 32],
-    [0, 3, 6, -9, 90, 20],
-    [1, 4, 7, 6, 90, 20],
-    [2, 5, 8, 22, 90, 20],
-    [0, 4, 8, 6, 45, 20],
-    [2, 4, 6, 6, -45, 20]]
+    let wins = [[0, 1, 2],
+    [3, 4, 5,],
+    [6, 7, 8, ],
+    [0, 3, 6, ],
+    [1, 4, 7,],
+    [2, 5, 8, ],
+    [0, 4, 8,],
+    [2, 4, 6, ]]
 
     wins.forEach((n) => {
         if ((boxtext[n[0]].innerText == boxtext[n[1]].innerText) && (boxtext[n[1]].innerText == boxtext[n[2]].innerText) && (boxtext[n[0]].innerText !== "")) {
             isgameover = true;
             document.querySelector(".info").innerText = `${boxtext[n[0]].innerText} Won`
             document.querySelector(".imgbox").getElementsByTagName("img")[0].style.width = "200px"
-            document.querySelector(".LINE").style.transform = `translateX${n[3]}vw rotate${n[4]}deg`
-            document.querySelector(".LINE").style.top = `${n[5]}%`
-            document.querySelector(".LINE").style.width = "40%"
-            document.querySelector(".LINE").style.border = "2px solid black"
+            gameover.play();
 
         }
 
     })
 }
-
+music.play();
 let box = document.querySelectorAll(".box")
 Array.from(box).forEach((element) => {
     element.addEventListener("click", () => {
         let boxtext = element.querySelector(".boxtext")
         if (boxtext.innerText == "") {
             boxtext.innerText = turn;
+            audioturn.play();
             turn = changeturn();
             checkforwin();
             if (!isgameover) {
@@ -68,8 +67,7 @@ reset.addEventListener("click", () => {
         isgameover = false
         document.querySelector(".info").innerText = `Turn for ${turn}`
         document.querySelector(".imgbox").getElementsByTagName("img")[0].style.width = "0px"
-        document.querySelector(".LINE").style.width = "0%"
-        document.querySelector(".LINE").style.border = "none"
+    
 
     }
 
